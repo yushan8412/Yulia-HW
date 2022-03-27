29bd58b1bcfcea8d952fd08e9940c4d1fab90dfd
yulia <ws61288412@gmail.com>
2022-03-28 00:05:27 +0800

    uplode screenshot

--- week1-5.playground/Contents.swift
+++ week1-5.playground/Contents.swift
@@ -1,12 +1,11 @@
-var jobLevels = [1 :"Member", 2 :"Team Leader", 3 : "Manager", 4 : "Director"]
 
-var jobLevel = 1
+// Function
+// 1. Please declare a function named greet with person as argument label and .name. as a parameter name. This function will return a String. For example, if you call the function greet like this:
+// greet(person: "Luke")
+// It will return the string: “Hello, Luke”.
 
-print(jobLevels[jobLevel] ?? "we don't have this job")
+func greet(name:String) -> String { return"hello," + name + "!"
+}
+print(greet(name: "Luke"))
 
-
-
-
-
-
-
+//Please declare a function named multiply with two arguments a and b . This function won’t return any value and will only print out the result of a * b . Be noticed that we want to give argument b a default value of 10.
